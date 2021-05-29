4ccb696770e9d0e824ccdde6f7eef38d99380d10
Nursamad <bagishbekow@gmail.com>
2021-05-29 11:34:29 +0600

    firstcommit

---
+++ src/com/geektech/Main.java
@@ -0,0 +1,13 @@
+package com.geektech;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final String name = "Nursamad ";
+        int age = 17;
+
+        System.out.println(name + age);
+
+    }
+}
